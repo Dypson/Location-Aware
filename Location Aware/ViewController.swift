936b642a4e455051340f35038ecdd38a8e81fd0c
Deepson Khadka <khaddeep@oisu.edu>
2019-07-27 20:41:33 -0400

    Initial and Final Commit

--- Location Aware/ViewController.swift
+++ Location Aware/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    @IBOutlet weak var lblDisplay: UILabel!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate=self
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+       // locationManager.requestLocation()
+        locationManager.startUpdatingLocation()
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var nearestAddress = ""
+        
+       /* print("Altitude: " + String(locations[0].altitude))
+        print("Speed: " + String(locations[0].speed))
+        print("Course: "+String(locations[0].course))
+        print("Latitude:"+String(locations[0].coordinate.latitude))
+        print("Longitude: "+String(locations[0].coordinate.longitude)) */
+        
+        CLGeocoder().reverseGeocodeLocation(locations[0]) {(placemarks,error) in
+            if error != nil{
+                 print("Error Found")
+            }else{
+                nearestAddress=(placemarks?[0].subThoroughfare!)!+" "+(placemarks?[0].thoroughfare)!+"\n"+(placemarks?[0].subAdministrativeArea)!+"\n"+(placemarks?[0].postalCode)!+"\n"+(placemarks?[0].country)!
+               //print(nearestAddress)
+          self.lblDisplay.text=nearestAddress
 
+            }
+            
+        }
+       
+    }
 
 }
 
