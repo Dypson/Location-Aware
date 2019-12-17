962cd6d8d07c032165a16d6b111eadaf43ff51e4
Deepson Khadka <khaddeep143@gmail.com>
2019-12-17 00:27:00 -0700

    Added Sound and VIbration Code

--- Finding User Locations/ViewController.swift
+++ Finding User Locations/ViewController.swift
@@ -7,6 +7,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController,CLLocationManagerDelegate {
 
@@ -29,7 +31,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
        // print("My location is: "+String(location.speed))
-       
+       let systemSoundID: SystemSoundID = 1016
         let _maxSpeed: Double = Double(finalSpeed.trimmingCharacters(in: .whitespaces))!//Double(lblInput.text!)!
         let mySpeed: Double = Double(location.speed)
         print("Maximum Speed Limit: "+finalSpeed+" mph")
@@ -37,6 +39,9 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
             lblDisplay.text=String(location.speed)+" mph"
              print("Current Speed: "+String(location.speed)+" mph")
             }else{
+            // to play sound
+            AudioServicesPlaySystemSound (systemSoundID)
+             AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
             print("Speed Limit Crossed: "+String(location.speed)+" mph")
             }
         self.map.setRegion(region, animated: true)
